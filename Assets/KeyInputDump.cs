6620663c592a7c5773b9817097680c402612a4d3
james7132 <james.siqi.liu@gmail.com>
2018-02-07 21:04:42 -0800

    Inital Commit

---
+++ Assets/KeyInputDump.cs
@@ -0,0 +1,29 @@
+﻿using System; 
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyInputDump : MonoBehaviour {
+
+  public Text Text;
+  KeyCode[] KeyCodes;
+
+  /// <summary>
+  /// Awake is called when the script instance is being loaded.
+  /// </summary>
+  void Awake() {
+     KeyCodes = (KeyCode[])Enum.GetValues(typeof(KeyCode));
+  }
+
+  /// <summary>
+  /// Update is called every frame, if the MonoBehaviour is enabled.
+  /// </summary>
+  void Update() {
+    Text.text = string.Join(" ", (from keycode in KeyCodes
+                                  where Input.GetKey(keycode)
+                                  select keycode.ToString()).ToArray());
+  }
+
+}
